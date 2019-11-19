0079b0a15d7bdcebd88b1266205f33bbe9d24e67
HZhen <huangzhen@17m.club>
2019-11-19 18:29:51 +0800

    新增一个多折线图

--- src/views/line-chart/index.vue
+++ src/views/line-chart/index.vue
@@ -31,6 +31,11 @@
           <line-chart5></line-chart5>
         </el-card>
       </el-col>
+      <el-col :span="12">
+        <el-card>
+          <line-chart6></line-chart6>
+        </el-card>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -41,6 +46,7 @@ import lineChart2 from './components/line-chart2/index'
 import lineChart3 from './components/line-chart3/index'
 import lineChart4 from './components/line-chart4/index'
 import lineChart5 from './components/line-chart5/index'
+import lineChart6 from './components/line-chart6/index'
 export default {
   name:'',
   components: {
@@ -49,7 +55,8 @@ export default {
     lineChart2,
     lineChart3,
     lineChart4,
-    lineChart5
+    lineChart5,
+    lineChart6
   },
   data () {
     return {
